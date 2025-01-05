708b199a4d4472632f818e6a34a3e32b0290b15a
riteshchavan111 <riteshchavan9637@gmail.com>
2025-01-05 15:46:56 +0530

    etl code added

---
+++ etl.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+data = {
+        'Task': ['Extract', 'Transform', 'Load'],
+        'Status': ['Completed', 'InProgress', 'Pending']
+    }
+
+df = pd.DataFrame(data)
+
+st.write('ETL Pipeline Execution Status', df)
+
